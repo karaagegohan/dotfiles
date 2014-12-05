56c8be99355775adc34307507e7f4866dcfdc67e
karaagegohan <usahillbanana@gmail.com>
2014-12-06 00:42:36 +0900

    vim.sh

--- vim.sh
+++ vim.sh
@@ -3,6 +3,5 @@
 apt-get install git
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-git clone https://github.com/karaagegohan/dotfiles ~/dotfiles
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gvimrc ~/.gvimrc
