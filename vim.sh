ea5b2f45e89c64fccff3da507459099008f943b8
karaagegohan <usahillbanana@gmail.com>
2014-12-06 00:40:38 +0900

    vim.sh

--- vim.sh
+++ vim.sh
@@ -5,5 +5,4 @@ mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 git clone https://github.com/karaagegohan/dotfiles ~/dotfiles
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-mkdir .vim/colors
-ln -s ~/dotfiles/.vim/colors ~/.vim/colors
+ln -s ~/dotfiles/.gvimrc ~/.gvimrc
