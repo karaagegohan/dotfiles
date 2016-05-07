36c096b8100df0fc632e192bb82c0ca93b6ee30d
AkiraYoneoka <Yoneoka@yoneokaryous-MacBook-Air.local>
2016-05-07 23:06:26 +0900

    update

--- install.sh
+++ install.sh
@@ -7,21 +7,21 @@ GITHUB_URL=https://github.com/ynoca/dotfiles
 if which "git"; then
     git clone --recursive "$GITHUB_URL" "$DOTPATH"
 
-# 使えない場合は curl か wget を使用する
-elif which "curl" || has "wget"; then
-    tarball=GITHUB_URL"/archive/master.tar.gz"
-    
-    # どっちかでダウンロードして，tar に流す
-    if which "curl"; then
-        curl -L "$tarball"
-
-    elif which "wget"; then
-        wget -O - "$tarball"
-
-    fi | tar xv -
-    
-    # 解凍したら，DOTPATH に置く
-    mv -f dotfiles-master "$DOTPATH"
+# # 使えない場合は curl か wget を使用する
+# elif which "curl" || has "wget"; then
+#     tarball=GITHUB_URL"/archive/master.tar.gz"
+#     
+#     # どっちかでダウンロードして，tar に流す
+#     if which "curl"; then
+#         curl -L "$tarball"
+#
+#     elif which "wget"; then
+#         wget -O - "$tarball"
+#
+#     fi | tar xv -
+#     
+#     # 解凍したら，DOTPATH に置く
+#     mv -f dotfiles-master "$DOTPATH"
 
 else
     die "curl or wget required"
