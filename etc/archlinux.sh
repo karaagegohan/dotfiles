547d9e9d32332ea078e152874710706e7c3c1589
ynoca <usahillbanana@gmail.com>
2016-07-25 16:05:36 +1000

    update

--- etc/archlinux.sh
+++ etc/archlinux.sh
@@ -3,14 +3,20 @@ has() { #{{{
 }
 #}}}
 
-if !(has go)
+if has go
 then
+    :
+else
     sudo pacman -S go
 fi
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 export GOPATH=$HOME/go
 
-if !(has peco)
+if has peco
 then
+    :
+else
     go get github.com/peco/peco/cmd/peco
 fi
+
+go get github.com/peco/peco/cmd/peco
