22af184f085f55988088f1728a34c167195c3d9e
ynoca <usahillbanana@gmail.com>
2017-02-17 03:08:24 +0900

    :tada: add comments

--- .zsh/znvim.zsh
+++ .zsh/znvim.zsh
@@ -1,7 +1,6 @@
+# current directory is changed automatically in neovim terminal
 neovim_autocd() {
     [[ $NVIM_LISTEN_ADDRESS ]] && $HOME/.zsh/neovim_autocd
 }
 chpwd_functions+=( neovim_autocd )
-
-# vim: set filetype=zsh:
 
